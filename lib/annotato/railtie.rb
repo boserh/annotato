e01af662915e9f90c3be2368aa9f1b876b382f69
Serhii Bodnaruk <sergiwez@gmail.com>
2025-05-17 17:41:30 +0300

    add after migration run

--- lib/annotato/railtie.rb
+++ lib/annotato/railtie.rb
@@ -5,7 +5,14 @@ require "rails/railtie"
 module Annotato
   class Railtie < ::Rails::Railtie
     rake_tasks do
+      # Load all annotato rake tasks as before
       Dir[File.expand_path("../tasks/**/*.rake", __dir__)].each { |f| load f }
+
+      # After db:migrate finishes, run annotato:models automatically
+      Rake::Task["db:migrate"].enhance do
+        Rake::Task["annotato:models"].reenable
+        Rake::Task["annotato:models"].invoke
+      end
     end
   end
 end
